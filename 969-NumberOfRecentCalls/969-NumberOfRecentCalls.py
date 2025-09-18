9920370c928d12049e936db58aa0b2418411469a
Anusha Upadya <89523888+anushaupadya7@users.noreply.github.com>
2025-09-19 00:16:47 +0530

    Create 969-NumberOfRecentCalls

---
+++ 969-NumberOfRecentCalls/969-NumberOfRecentCalls.py
@@ -0,0 +1,10 @@
+# Last updated: 19/09/2025, 00:16:47
+class RecentCounter:
+    def __init__(self):
+        self.queue = deque()  
+
+    def ping(self, t: int) -> int:
+        self.queue.append(t)  
+        while self.queue[0] < t - 3000:
+            self.queue.popleft()
+        return len(self.queue) 
