b53a4c99b4823ce8ef73015bda188f83dcbbaf50
Anusha Upadya <89523888+anushaupadya7@users.noreply.github.com>
2025-09-26 00:00:51 +0530

    Create 875-KokoEatingBananas

---
+++ 875-KokoEatingBananas/875-KokoEatingBananas.py
@@ -0,0 +1,15 @@
+# Last updated: 26/09/2025, 00:00:52
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        left,right=1,max(piles)
+
+        while left<right:
+            mid=(left+right)//2
+            total_hours=sum(math.ceil(p/mid) for p in piles) 
+
+            if total_hours > h: 
+                left = mid + 1
+            else:  
+                right = mid
+
+        return left 
