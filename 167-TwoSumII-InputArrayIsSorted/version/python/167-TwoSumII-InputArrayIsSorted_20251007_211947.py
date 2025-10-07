a327f5d4b157f3a35c285509b531fa7f8978539c
Anusha Upadya <89523888+anushaupadya7@users.noreply.github.com>
2025-10-07 21:19:47 +0530

    Create 167-TwoSumII-InputArrayIsSorted

---
+++ 167-TwoSumII-InputArrayIsSorted/version/python/167-TwoSumII-InputArrayIsSorted_20251007_211947.py
@@ -0,0 +1,20 @@
+# Last updated: 07/10/2025, 21:19:47
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        # store={}
+        # for i,num in enumerate(numbers):
+        #     complement=target-num
+        #     if complement in store:
+        #         return [store[complement]+1,i+1]
+        #     store[num]=i
+        # return []
+
+        left,right=0,len(numbers)-1
+        while(left<right):
+            current_sum=numbers[left]+numbers[right]
+            if current_sum==target:
+                return [left+1,right+1]
+            elif current_sum<target:
+                left+=1
+            else:
+                right-=1
